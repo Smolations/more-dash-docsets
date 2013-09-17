d190790762ed929bd1d829f992d4e519710b1705
Chris Smola <csmola@finishline.com>
2013-09-17 00:29:02 -0600

    (master) put puppet generator in appropriate file

--- generators/_gen_dash_docs_puppet.rb
+++ generators/_gen_dash_docs_puppet.rb
@@ -1,38 +1,213 @@
+require File.join(File.dirname(__FILE__), 'Dash.rb')
 
-classes_list = "index.dsp"
+dash = Dash.new({
+    :name           => 'Puppet',
+    :display_name   => 'Puppet 3.2',
+    :docs_root      => 'puppetdocs-latest',
+    :icon           => File.join('icon-images', 'puppetlabs.png')
+})
+# so dive can get at it recursively..
+$dash = dash
 
-# (?<=href=")\/((?:[a-z0-9.]+\/)*)([a-z0-9]+)(?=")
-# file:///Users/cos/Development/workspaces/dash-docs/puppetdocs-latest/$1$2/index.html
 
-File.open(classes_list, 'r') do |file|
-    count   = 0
-    queries = []
+# won't need to touch these files
+entries = dash.get_clean_docs_entries([
+    'config.ru', 'favicon.ico', 'module_cheat_sheet.pdf', 'puppet_core_types_cheatsheet.pdf',
+    'README.txt', 'sitemap.xml', 'images'
+])
+# this is how we'll keep track of how deep in the directory structure we are as we make paths
+# relative. this array is joined with the system file separator and prepended to absolute paths.
+$levels = [ '.' ]
 
-    file.each_line do |line|
+# shortcut method to increase/decrease levels.
+def level_up
+    $levels.push('/..')
+end
+def level_down
+    $levels.pop
+end
 
-        line = line.chomp
-        matches = /^s\d+dsp([a-z]+)01$/.match(line)
+$c = true
+$fileCount = 0
+def dive(path)
+    # puts "#{path}"
+    entries = $dash.clean_dir_entries(path, ['latest', 'images', 'assets', 'fonts'])
 
-        if !matches.nil?
-            tag_name = "dsp:#{matches[1]}"
-            html_path = "#{line}.html"
 
-            queries << "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (\"#{tag_name}\", \"Tag\", \"#{html_path}\");"
+    entries.each do |entry|
+        entry_path = File.join(path, entry)
+
+        # if we're looking at a directory and not a file [to parse], increase the directory
+        # depth, pass the path to this function, then decrease the level back to where it was.
+        if File.directory?(entry_path)
+            level_up
+            dive(entry_path)
+            level_down
+
+        # now that we know it's a file, let's get into it.
+        elsif !entry.match(/\.html$/).nil?
+            $fileCount = $fileCount + 1
+            doc = $dash.get_noko_doc(entry_path)
+            doc.css('[href]').each {|element| element['href'].match(/^\//) && element['href'] = $levels.join + element['href'] }
+            doc.css('[src]').each {|element| element['src'].match(/^\//) && element['src'] = $levels.join + element['src'] }
+            $dash.save_noko_doc(doc, entry_path)
+            # if $c
+                # fix relative paths for src="" and href="" attributes
+                # `sed -i '' -e 's: href="/: href="#{$levels.join}/:' #{entry_path}`
+                # `sed -i '' -e "s: href='/: href='#{$levels.join}/:" #{entry_path}`
+                # `sed -i '' -e 's: src="/: src="#{$levels.join}/:' #{entry_path}`
+                # `sed -i '' -e "s: src='/: src='#{$levels.join}/:" #{entry_path}`
+
+                # remove google analytics and tracking
+                `sed -i '' -e '/<!-- Google analytics -->/,/<!-- End Google analytics -->/d' #{entry_path}`
+                `sed -i '' -e '/<!-- BEGIN: MARKETO TRACKING -->/,/<!-- END: MARKETO TRACKING -->/d' #{entry_path}`
+                # $c = false
+            # end
+            # puts "in (#{entry_path})  href=\"#{$levels.join}/files/css....."
+            # `echo "in (#{entry_path})  href=\"#{$levels.join}/files/css....."`
+            # parse html
+
         end
-
-        # html_file.close
-        count = count + 1
-
-    # /file.each_line
     end
 
-    puts "\nProcessed a total of #{count} lines."
-    puts "There are #{queries.length} queries to perform.\n"
-    qs = queries.join("\n")
-    # puts "\n#{qs}"
-    queries.each {|query| `cd DSP.docset/Contents/Resources/; sqlite3 docSet.dsidx '#{query}'`}
-
-    # puts queries.join("\n")
-
-# close classes_list
 end
+
+
+# kick off the function to make href and src paths relative
+puts "Relative-izing src/href attributes and removing analytics..."
+dive(dash::docs_root)
+puts " \`-Done processing #{$fileCount} files."
+
+
+
+# load up guides
+puts "Processing guides..."
+guides_path = File.join(dash::docs_root, 'guides')
+guides      = dash.clean_dir_entries(guides_path)
+cnt         = 0
+
+guides.each do |entry|
+    entry_path  = File.join(guides_path, entry)
+    sql_path    = File.join('guides', entry)
+
+    if !File.directory?(entry_path)
+        doc     = $dash.get_noko_doc(sql_path)
+        # the split character had to be copied and pasted from one of the docs. it is &#8212;
+        name    = doc.at_css('title').content.split('—').shift.strip
+        type    = 'Guide'
+        if !name.nil?
+            cnt = cnt + 1
+            $dash.sql_insert(name, type, sql_path)
+        end
+    end
+end
+puts " \`- Done processing #{cnt} files."
+
+
+# the following pages are all versioned. we sync versions here.
+refs_version    = 'latest'
+# refs_version = '3.2.3'    # if a specific version is desired
+
+# Types
+puts "Processing types reference..."
+cnt         = 0
+refs_page   = 'type.html'
+sql_path    = File.join('references', refs_version, refs_page)
+
+docRef      = dash.get_noko_doc(sql_path)
+# e.g. <li class="toc-lv2"><a href="#type">
+docRef.css('.toc-lv3 a').each do |a|
+    cnt     = cnt + 1
+    path    = sql_path + a['href']
+    name    = path.split('#').pop
+    type    = 'Type'
+    dash.sql_insert(name, type, path)
+end
+puts " \`- Done processing #{cnt} Type anchors."
+
+
+# Functions
+puts "Processing Function reference..."
+cnt         = 0
+refs_page   = 'function.html'
+sql_path    = File.join('references', refs_version, refs_page)
+
+docRef    = dash.get_noko_doc(sql_path)
+# e.g. <li class="toc-lv2"><a href="#function">
+docRef.css('.toc-lv2 a').each do |a|
+    cnt   = cnt + 1
+    path  = sql_path + a['href']
+    name  = path.split('#').pop
+    type  = 'Function'
+    dash.sql_insert(name, type, path)
+end
+puts " \`- Done processing #{cnt} Function anchors."
+
+
+# Metaparameters
+puts "Processing Metaparameter reference..."
+cnt         = 0
+refs_page   = 'metaparameter.html'
+sql_path    = File.join('references', refs_version, refs_page)
+
+docRef    = dash.get_noko_doc(sql_path)
+# e.g. <li class="toc-lv3"><a href="#metaparam">
+docRef.css('.toc-lv3 a').each do |a|
+    cnt   = cnt + 1
+    path  = sql_path + a['href']
+    name  = path.split('#').pop
+    type  = 'Parameter'
+    dash.sql_insert(name, type, path)
+end
+puts " \`- Done processing #{cnt} Metaparameter anchors."
+
+
+# Commands with the `puppet` binary
+puts "Processing \`puppet\` binary commands..."
+cnt     = 0
+entries = dash.clean_dir_entries( File.join(dash::docs_root, 'man') )
+
+# each command has its own html page. notice that this collection of commands
+# is simple enough to only use one Dash feature.
+entries.each do |entry|
+    cnt = cnt + 1
+    name = entry.split('.').shift
+    type = 'Command'
+    path = File.join('man', entry)
+    $dash.sql_insert(name, type, path)
+end
+puts " \`- Done processing #{cnt} Commands."
+
+
+# Facter: Core Facts
+cnt             = 0
+refs_version    = '1.7'
+refs_page       = 'core_facts.html'
+refs_name       = 'facter'
+sql_path        = File.join(refs_name, refs_version, refs_page)
+
+docRef          = dash.get_noko_doc(sql_path)
+# e.g. <li class="toc-lv2"><a href="#fact">
+docRef.css('.toc-lv2 a').each do |a|
+    cnt         = cnt + 1
+    path        = sql_path + a['href']
+    name        = path.split('#').pop
+    type        = 'Global'
+    dash.sql_insert(name, type, path)
+end
+puts " \`- Done processing #{cnt} Facter facts."
+
+# dash.sql_execute({
+#     :noop => true,
+#     :filter => {
+#         :limit => 5,
+#         :type => 'Class',
+#         :name => 'Exception'
+#     }
+# })
+dash.sql_execute
+
+# dash.copy_docs(:noop => true)
+dash.copy_docs()
+
+puts "\nDone."
